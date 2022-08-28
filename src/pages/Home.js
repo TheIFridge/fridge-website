6e792610036f397f07acfb3d00aa8bf03f7e84dd
bombard21 <wangxiaofeng153@gmail.com>
2022-08-28 18:27:29 +1200

    Recipes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,19 +5,9 @@ import React from 'react';
 export default function Home() {
     return (
         <div>
-            <img src = {'./img/icons/old/favicon-196x196.png'} alt = "logo" />
-            {{/*bender*/}}
-            <style jsx>{`
-                img {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                }
-            `}</style>
-
-
             <h1>Ifridge</h1>
-
+            <p>Welcome To Firdge App</p>
+            
         </div>
     );
 }
