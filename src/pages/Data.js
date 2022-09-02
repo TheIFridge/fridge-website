b5581004708f4a052eb7967ca3a069d85ceed248
bombard21 <wangxiaofeng153@gmail.com>
2022-09-02 15:29:40 +1200

    Recipes Version 3

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -6,7 +6,7 @@ const Data = [
         "description": "yellow lentils with spices",
         "ingredients": "lentils,spcies,oil,salt,pepper,garlic,onion",
         "allergies": "vegetarian",
-        "image": "https://c.ndtvimg.com/2021-04/37hi8sl_rava-upma_625x300_17_April_21.jpg?im=FeatureCrop,algorithm=dnn,width=620,height=350"
+        "image": "https://www.countdown.co.nz/Content/Recipes/REP_Blue-Cheese-Tart.jpg"
     },
     {
         "id": 2,
@@ -15,7 +15,7 @@ const Data = [
         "description": "Curried Lamb With Spices",
         "ingredients": "lentils,spcies,oil,salt,pepper,garlic,onion",
         "allergies": "vegetarian",
-        "image": "https://i.ndtvimg.com/i/2015-06/indian-dinner_625x350_41434360207.jpg"
+        "image": "https://www.countdown.co.nz/Content/Recipes/Chicken-Casserole540.jpg"
     },
     {
         "id": 3,
@@ -24,7 +24,7 @@ const Data = [
         "description": "Curried Paneer",
         "ingredients": "lentils,spcies,oil,salt,pepper,garlic,onion",
         "allergies": "vegetarian",
-        "image": "https://i.ndtvimg.com/i/2015-04/paneer_625x350_61429707960.jpg"
+        "image": "https://www.countdown.co.nz/Content/Recipes/227490%20-%20Fettuccine%20Old%20Recipes%20Reshoot%20810x570.jpg"
     },
 ];
 export default Data;
--- src/pages/Recepies.js
+++ src/pages/Recepies.js
@@ -4,160 +4,67 @@ import Card from 'react-bootstrap/Card';
 import { Button } from 'react-bootstrap';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
-import { Link } from 'react-router-dom';
 import Data from './Data';
+import Stack from 'react-bootstrap/Stack';
+import '../style/Recipes.css'
+
+function Buttons() {
+  // grab the data from the Data.js file
+  const data = Data;
+  //if the data Category is equal to the button name, then return the data
+  const vegetarian = data.filter((item) => item.category === "Vegetarian");
+  const nonVegetarian = data.filter((item) => item.category === "Non-Vegetarian");
+}
 
 
-// main function
-const Recepies = () => {
-  const [data, setData] = React.useState(Data);
-  const menuItems = [...new Set(data.map(item => item.category))];
-
-  const filterItems = (category) => {
-    const newItem = Data.filter((newVal) => {
-      return newVal.category === category;
-    });
-    setData(newItem);
-  };
+export default function Recepies() {
   return (
     <>
-    <div className="container-fluid">
-        <div className="row">
-          <h1 className="col-12 text-center my-3 fw-bold">Food Filtering App</h1>
-          <Buttons
-            filterItem={filterItem}
-            setItem={setItem}
-            menuItems={menuItems}
-          />
-          <Card item={item} />
-        </div>
+      <div> 
+        {/* Buttons */}
+        <Stack direction="horizontal" gap={3}>
+          <Button variant="primary" size="lg" onClick={Buttons}>
+            Vegetarian
+          </Button>
+          <Button variant="primary" size="lg" onClick={Buttons}>
+            Non-Vegetarian
+          </Button>
+          <Button variant="primary" size="lg" onClick={Buttons}>
+            DairyFree
+          </Button>
+        </Stack>
+        <br></br>
+
+        {/* Cards */}
+        <Row>
+          <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://www.countdown.co.nz/Content/Recipes/Chicken-Casserole540.jpg/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          </Col>
+          <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://www.countdown.co.nz/Content/Recipes/REP_Blue-Cheese-Tart.jpg/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          </Col>
+        </Row>
       </div>
-   </>
-  );
-};
-
-export default Recepies;
-
-// return (
-//   <div>
-//     <h1>Recipes</h1>
-//     <p>For all your delicious foods</p>
-//     {/*button that filters and displays vegeterian meals*/}
-//     <Button variant="primary" size="lg" block>
-//       Vegetarian
-//     </Button>
-
-
-
-//     {/*cards for each diffirent meal*/}
-//     <Row xs={1} md={2} className="g-4">
-//       {Array.from({ length: 1 }).map((_, idx) => (
-//         <Col>
-//           <Card style={{ width: '18rem' }}>
-//             <Card.Img variant="top" src="https://c.ndtvimg.com/2021-04/37hi8sl_rava-upma_625x300_17_April_21.jpg?im=FeatureCrop,algorithm=dnn,width=620,height=350" />
-//             <Card.Body>
-//               <Card.Title>Dal</Card.Title>
-//               <Card.Text>
-//                 yellow lentils with spices
-//               </Card.Text>
-
-//               {/* button that will bring up a diffirent recipe for each meal*/}
-//               <Link to='/Recipe'>
-//                 <Button variant="primary">Go somewhere</Button>
-//                 {/*onclick display the vegeterian cards*/}
-//               </Link>
-//             </Card.Body>
-//           </Card>
-//         </Col>
-//       ))}
-
-//       {Array.from({ length: 1 }).map((_, idx) => (
-//         <Col>
-//           <Card style={{ width: '18rem' }}>
-//             <Card.Img variant="top" src="https://i.ndtvimg.com/i/2015-06/indian-dinner_625x350_41434360207.jpg" />
-//             <Card.Body>
-//               <Card.Title>Lamb Curry</Card.Title>
-//               <Card.Text>
-//                 Curried Lamb With Spices
-//               </Card.Text>
-//               <Link to='/Recipe'>
-//                 <Button variant="primary">Go somewhere</Button>
-//               </Link>
-//             </Card.Body>
-//           </Card>
-//         </Col>
-//       ))}
-//     </Row>
-//     <br>
-//     </br>
-//     <Row xs={1} md={2} className="g-4">
-//       {Array.from({ length: 1 }).map((_, idx) => (
-//         <Col>
-//           <Card style={{ width: '18rem' }}>
-//             <Card.Img variant="top" src="https://i.ndtvimg.com/i/2015-04/paneer_625x350_61429707960.jpg" />
-//             <Card.Body>
-//               <Card.Title>Paneer</Card.Title>
-//               <Card.Text>
-//                 Curried Paneer
-//               </Card.Text>
-//               <Link to='/Recipe'>
-//                 <Button variant="primary">Go somewhere</Button>
-//               </Link>
-//             </Card.Body>
-//           </Card>
-//         </Col>
-//       ))}
-//       {Array.from({ length: 1 }).map((_, idx) => (
-//         <Col>
-//           <Card style={{ width: '18rem' }}>
-//             <Card.Img variant="top" src="https://i.ndtvimg.com/i/2017-10/makhmali-kofte_620x350_51508918483.jpg" />
-//             <Card.Body>
-//               <Card.Title>Kofta</Card.Title>
-//               <Card.Text>
-//                 stewed Kofta
-//               </Card.Text>
-//               <Link to='/Recipe'>
-//                 <Button variant="primary">Go somewhere</Button>
-//               </Link>
-//             </Card.Body>
-//           </Card>
-//         </Col>
-//       ))}
-//     </Row>
-//     <br>
-//     </br>
-//     <Row xs={1} md={2} className="g-4">
-//       {Array.from({ length: 1 }).map((_, idx) => (
-//         <Col>
-//           <Card style={{ width: '18rem' }}>
-//             <Card.Img variant="top" src="https://i.ndtvimg.com/i/2017-10/makhmali-kofte_620x350_51508918483.jpg" />
-//             <Card.Body>
-//               <Card.Title>Kofta</Card.Title>
-//               <Card.Text>
-//                 stewed Kofta
-//               </Card.Text>
-//               <Link to='/Recipe'>
-//                 <Button variant="primary">Go somewhere</Button>
-//               </Link>
-//             </Card.Body>
-//           </Card>
-//         </Col>
-//       ))}
-//       {Array.from({ length: 1 }).map((_, idx) => (
-//         <Col>
-//           <Card style={{ width: '18rem' }}>
-//             <Card.Img variant="top" src="https://i.ndtvimg.com/i/2015-06/indian-dinner_625x350_41434360207.jpg" />
-//             <Card.Body>
-//               <Card.Title>Lamb Curry</Card.Title>
-//               <Card.Text>
-//                 Curried Lamb With Spices
-//               </Card.Text>
-//               <Link to='/Recipe'>
-//                 <Button variant="primary">Go somewhere</Button>
-//               </Link>
-//             </Card.Body>
-//           </Card>
-//         </Col>
-//       ))}
-//     </Row>
-//   </div>
+    </>
+  )
+}
