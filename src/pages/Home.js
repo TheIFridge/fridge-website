37a8c319d49cc75f346e829750bef356f850716f
bombard21 <wangxiaofeng153@gmail.com>
2022-09-06 14:38:18 +1200

    images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,45 @@
 // react
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import { Button } from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 // main function
 export default function Home() {
     return (
         <>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-            </Card>
+            <Form>
+                <Row>
+                    <Col>
+                        <h1>
+                            Welcome to the Recipe App
+                        </h1>
+                    </Col>
+                    <Col>
+                        <Card border="light" style={{ width: '18rem' }} >
+                            <Card.Img variant="top" src="img/pages/home/fridge.png" />
+                        </Card>
+                    </Col>
+                </Row>
+                <br></br><br></br>
+                <Row>
+                    <Col>
+                    <Card bg = "light" border="light" style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="img/pages/home/fridgeApp.png" />
+                        </Card>
+                    </Col>
+                    <Col>
+                        <h1>
+                            This is the App
+                        </h1>
+                        <p>
+                            A resposnive web app that allows you to search for recipes based on the ingredients you have in your fridge.
+                            
+                        </p>
+                    </Col>
+                </Row>
+            </Form>
         </>
     );
 }
