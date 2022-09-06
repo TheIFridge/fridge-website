1de990a9de07da74dabdc8412d0de0316d67957f
bombard21 <wangxiaofeng153@gmail.com>
2022-09-06 12:58:12 +1200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 // react
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Button } from 'react-bootstrap';
 
 // main function
 export default function Home() {
     return (
-        <div>
-            <h1>Ifridge</h1>
-            <p>Welcome To Firdge App</p>
-            
-        </div>
+        <>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+            </Card>
+        </>
     );
 }
