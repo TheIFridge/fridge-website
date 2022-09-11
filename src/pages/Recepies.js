5895ee207984e9b68a84a76353c135b84fd4f8c4
bombard21 <wangxiaofeng153@gmail.com>
2022-09-11 16:18:49 +1200

    Code

--- src/pages/Recepies.js
+++ src/pages/Recepies.js
@@ -4,64 +4,90 @@ import Card from 'react-bootstrap/Card';
 import { Button } from 'react-bootstrap';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
-import Data from './Data';
-import Stack from 'react-bootstrap/Stack';
 import '../style/Recipes.css'
 
-function Buttons() {
-  // // grab the data from the Data.js file
-  // const data = Data;
-  // //if the data Category is equal to the button name, then return the data
-  // const vegetarian = data.filter((item) => item.category === "Vegetarian");
-  // const nonVegetarian = data.filter((item) => item.category === "Non-Vegetarian");
-}
 
 
 export default function Recepies() {
   return (
     <>
-      <div> 
-        {/* Buttons */}
-        <Stack direction="horizontal" gap={3}>
-          <Button variant="primary" size="lg" onClick={Buttons}>
-            Vegetarian
-          </Button>
-          <Button variant="primary" size="lg" onClick={Buttons}>
-            Non-Vegetarian
-          </Button>
-          <Button variant="primary" size="lg" onClick={Buttons}>
-            DairyFree
-          </Button>
-        </Stack>
-        <br></br>
-
+      <div>
         {/* Cards */}
         <Row>
           <Col>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://www.countdown.co.nz/Content/Recipes/Chicken-Casserole540.jpg/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="https://www.countdown.co.nz/Content/Recipes/227490_ButterChicken_large_810x570.jpg" />
+              <Card.Body>
+                <Card.Title>Butter Chicken</Card.Title>
+                <Card.Text>
+                  You forget about takeaways with this easy and delicious recipe for Butter Chicken! This curry favourite is easy to prepare and ready in 30 minutes.
+                </Card.Text>
+                <Button  href="./Recipe" variant="primary">Recipe</Button>
+              </Card.Body>
+            </Card>
           </Col>
           <Col>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://www.countdown.co.nz/Content/Recipes/REP_Blue-Cheese-Tart.jpg/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="https://www.countdown.co.nz/Content/Recipes/227490_Herb-InfusedRoastLamb_large_810x570.jpg" />
+              <Card.Body>
+                <Card.Title>Herb-Infused Roast Lamb</Card.Title>
+                <Card.Text>
+                  But any day is perfect to try this recipe for Herb-Infused Roast Lamb, which is even better when seasoned the night before, to let those flavours really infuse.
+                </Card.Text>
+                <Button href="./Recipe" variant="primary">Recipe</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="https://www.countdown.co.nz/Content/Recipes/Large_751.jpg" />
+              <Card.Body>
+                <Card.Title>Baked Salmon with Tomato Salsa and Crushed Potatoes</Card.Title>
+                <Card.Text>
+                  This recipe for Baked Salmon with Tomato Salsa and Crushed Potatoes is a great way to get your omega-3s and is ready in just 30 minutes.
+                </Card.Text>
+                <Button href="./Recipe" variant="primary">Recipe</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <br></br>
+        <Row>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="https://www.countdown.co.nz/Content/Recipes/Roasted-Baby-Carrot-Salad-540.jpg" />
+              <Card.Body>
+                <Card.Title>Roasted Baby Carrot Salad</Card.Title>
+                <Card.Text>
+                  This Roasted Baby Carrot Salad and is ready in just 30 minutes.
+                </Card.Text>
+                <Button href="./Recipe" variant="primary">Recipe</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="https://www.countdown.co.nz/Content/Recipes/Thai-Beef-and-Sweet-Potato-Curry540.jpg" />
+              <Card.Body>
+                <Card.Title>Thai Beef Curry</Card.Title>
+                <Card.Text>
+                  This Thai Beef and Sweet Potato Curry and is ready in just 30 minutes.
+                </Card.Text>
+                <Button href="./Recipe" variant="primary">Recipe</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="https://www.countdown.co.nz/Content/Recipes/bbq-corn-540.jpg" />
+              <Card.Body>
+                <Card.Title>BBQ Corn</Card.Title>
+                <Card.Text>
+                  This BBQ Corn is a great way to get your omega-3s and is ready in just 30 minutes.
+                </Card.Text>
+                <Button href="./Recipe" variant="primary">Recipe</Button>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </div>
---
+++ src/pages/Recipe.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Recepies from "./Recepies";
+import Data from "./Data";
+import * as React from "react";
+import * as ReactBootstrap from "react-bootstrap";
+
+
+
+export default function Recipe() {
+    return (
+        <>
+        <div>
+            <Recepies />
+            <Data />
+        </div>
+        </>
+    );
+
+}
