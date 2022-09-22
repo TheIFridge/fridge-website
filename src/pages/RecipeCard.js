4ef6f2a037f151ad54e603e424fce14d8a113e9a
bombard21 <wangxiaofeng153@gmail.com>
2022-09-22 20:56:21 +1200

    Fixed Recipes

--- src/pages/RecipeCard.js
+++ src/pages/RecipeCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { useState } from "react";
-import { Row,Col } from "react-bootstrap";
 
 // import Data from './Data';
 
@@ -9,24 +8,49 @@ const RecipeCard = ({ item }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <div className="container-fluid">
-        <Row>
-          <Col>
+      <motion.div className="container-fluid">
+        <motion.div className="row justify-content-center">
           {item.map((Val) => {
-          return (
-              <div
-                className="col-md-4 col-sm-6 card my-3 py-3 border-0"
-                key={Val.id}
-              >
-                <motion.div onClick={() => setIsOpen(!isOpen)} className="card">
-                  <motion.h2>{Val.title}</motion.h2>
+            return (
+              
 
-                  <motion.div>
+
+              <motion.div onClick={() => setIsOpen(!isOpen)} className="col-md-4 col-sm-6 card my-3 py-3 border-0" key={Val.id}>
+                <motion.div className="card-img-top text-center">
+                  <img src={Val.images} alt={Val.title} className="photo w-75" />
+                </motion.div>
+                <motion.div className="card-body">
+                  <motion.div className="card-title fw-bold fs-4">
+                    {Val.title} &nbsp;&nbsp;&nbsp;&nbsp;--&nbsp;&nbsp;
+                    {Val.price}
+                  </motion.div>
+                  <motion.div className="card-text">{Val.desc}</motion.div>
+                  {isOpen &&
+                    <motion.div>
+                      {Val.method}
+                    </motion.div>
+                  }
+                </motion.div>
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
+    </>
+  )
+}
+
+
+export default RecipeCard;
+{/* <motion.div onClick={() => setIsOpen(!isOpen)} className="card">
+                  <motion.div className="card-img-top text-center">
                     <motion.img src={Val.images} alt={Val.title} className="photo w-75"></motion.img>
                     <motion.div>
-                      {Val.title}
-                      <br></br>
-                      {Val.price}
+                      <motion.div>
+                        {Val.title}
+                        <br></br>
+                        {Val.price}
+                      </motion.div>
                     </motion.div>
                   </motion.div>
                   {isOpen &&
@@ -34,31 +58,4 @@ const RecipeCard = ({ item }) => {
                       {Val.method}
                     </motion.div>
                   }
-
-                  {/* <div className="card-img-top text-center">
-                  <img src={Val.images} alt={Val.title} className="photo w-75" />
-                </div>
-                <div className="card-body">
-                  <div className="card-title fw-bold fs-4">
-                    {Val.title} &nbsp;&nbsp;&nbsp;&nbsp;--&nbsp;&nbsp;
-                    {Val.price}
-                  </div>
-                  <div className="card-text">{Val.desc}</div>
-                </div> */}
-                </motion.div>
-              </div>
-            );
-          })}
-          </Col>
-        
-        </Row>
-        {/* <div className="row justify-content-centre"> */}
-          
-        {/* </div> */}
-      </div>
-    </>
-  )
-}
-
-
-export default RecipeCard;
+                </motion.div> */}
