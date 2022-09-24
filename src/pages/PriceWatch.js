24e746bff3fe5cf03f4d95bcd2e7dbac9acb24b7
Nathan <nathand1278@gmail.com>
2022-09-24 19:28:26 +1200

    Update PriceWatch.js

--- src/pages/PriceWatch.js
+++ src/pages/PriceWatch.js
@@ -6,28 +6,10 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
 import { getIngredients } from '../util/Functions';
+import { capitalise } from '../util/Helpers';
 // import { userLoggedIn } from '../util/Helpers';
 
 export default function PriceWatch() {
-    // const [update, setUpdate] = useState(false);
-    // if (!userLoggedIn()) {
-    //     setUpdate(false);
-    // }
-
-    // if update, allow user to update price watch, else use database stuff
-
-    // const cardInfo = [
-    //     {image: "https://upload.wikimedia.org/wikipedia/commons/9/92/95apple.jpeg", title: "Apple", text: "Royal Gala Apples", price: "$2.50/kg"},
-    //     {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/440px-Banana-Single.jpg", title: "Banana", text: "Fresh Dole Green Banana", price: "$6.90/kg"},
-    //     {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Fresh_made_bread_05.jpg/500px-Fresh_made_bread_05.jpg", title: "Bread", text: "Wholemeal Sandwich Bread", price: "$2.50"},
-    //     {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/440px-Oranges_-_whole-halved-segment.jpg", title: "Oranges", text: "Fresh Oranges 6pk", price: "$4.20"},
-    //     {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Pears.jpg/440px-Pears.jpg", title: "Pears", text: "Green Pear", price: "$2.90/2ea"},
-    //     {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Abhar-iran.JPG/340px-Abhar-iran.JPG", title: "Grapes", text: "Red Seedless Grapes", price: "$3.60/pk"},
-    //     {image: "https://assets.woolworths.com.au/images/2010/146108.jpg?impolicy=wowcdxwbjbx&w=900&h=900", title: "Pineapple", text: "Pineapple Whole", price: "$4.50/ea"},
-    //     {image: "https://meadowfresh.co.nz/assets/Products/Milk-2L/MF_2L_ORIGINAL__FillWzYwMCw2MDBd.png", title: "Milk", text: "2L Milk Standard", price: "$3.50"},
-    //     {image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/7._carton_of_eggs-9436ea8.jpg?quality=90&fit=700,350", title: "Eggs", text: "12pk Free Range Chicken Eggs", price: "$10.30"},
-    // ];
-
     // create states for searchInput, and searchResults
     const [isLoading, setIsLoading] = useState(true);
     const [cardInfo, setCardInfo] = useState({});
@@ -50,16 +32,20 @@ export default function PriceWatch() {
         if (event.target.value === "") {
             if (!isLoading) setSearchResults(cardInfo);
         } else {
+            console.log("searching");
             // otherwise, filter the cardInfo to only include the searchInput
             const newFilter = cardInfo.filter((value) => {
-                return value.name.toLowerCase().includes(searchInput.toLowerCase());
+                // console.log(value.name.toLowerCase());
+
+
+                return value.name.toLowerCase().includes(searchInput.toLowerCase()) || value.generic_name.toLowerCase().includes(searchInput.toLowerCase());
             });
             setSearchResults(newFilter);
         }
     };
 
     const handleWatchlistAdd = (ingredient, event) => {
-        event.target.innerText = "Added to Watchlist!";
+        event.target.innerText = "Watching";
     }
 
     return (
@@ -72,20 +58,24 @@ export default function PriceWatch() {
                 </Form.Group>
             </Form>
             <br />
-            <Row xs={2} md={3} className="g-4">
+            <Row xs={1} md={3} className="g-4">
                 {searchResults.map((value, index) => {
                     return (
-                        <Col>
-                        {/* make sure card height takes up entire height */}
+                        <Col key={index}>
                             <Card style={{ "height": "100%" }}>
                                 {/* <Card.Img variant="top" src={value.image} /> */}
                                 <Card.Body>
-                                    <Card.Title>{value.generic_name}</Card.Title>
-                                    <Card.Text>{value.name}</Card.Text>
-                                    <Card.Text>{value.price ?? "Untracked"}</Card.Text>
-                                    {/* <Button variant="primary">Add to Watchlist</Button> */}
+                                    <Card.Title>{capitalise(value.generic_name)}</Card.Title>
+                                    <Card.Text>{capitalise(value.name)}</Card.Text>
+                                    <Card.Text>{value.price ?? "Price Untracked"}</Card.Text>
+                                    <hr/>
+                                    {/* 2 evenly spaced columns with placeholder text in each*/}
+                                    <Card.Text>{"Countdown: $0.00"}</Card.Text>
+                                    <Card.Text>{"Pak'N'Save: $0.00"}</Card.Text>
                                 </Card.Body>
-                                <Card.Footer><Button variant="primary" onClick={(event) => {handleWatchlistAdd(value, event)}}>Add to Watchlist</Button></Card.Footer>
+                                <Card.Footer>
+                                    <Button variant="primary" onClick={(event) => {handleWatchlistAdd(value, event)}}>Add to Watchlist</Button>
+                                </Card.Footer>
                             </Card>
                         </Col>
                     );
