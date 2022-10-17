6606430bcb5152ab8fd00bd983a1123a48425ca2
llwong03 <lachlan.ljwong@gmail.com>
2022-10-18 12:41:20 +1300

    Update Support.js

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -4,29 +4,34 @@ import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import { InputGroup } from 'react-bootstrap';
 import { useRef } from 'react';
+
 import emailjs from '@emailjs/browser';
 // import InputGroup from 'react-bootstrap/InputGroup';
 
 export default function Support() {
 
-    // const [name, setName] = useState('');
-	// const [email, setEmail] = useState('');
-    // const [subject, setSubject] = useState('');
-    // const [message, setMessage] = useState('');
+    const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
 
     const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
+
+        // console.log(e.target.user_name);
+        console.log(form.current);
     
-        emailjs.sendForm('service_li8u0xp', 'template_t4nrt2u', form.current, '2Db5whLZG7lX83Sc7')
+        emailjs.sendForm('service_li8ozfr', 'template_wijoygh', form.current, '2Db5whLZG7lX83Sc7')
           .then((result) => {
-              console.log(result.text);
+              console.log("resX: ", result.text);
           }, (error) => {
-              console.log(error.text);
+              console.error("errX: ", error.text);
           });
+
           e.target.reset();
-      };
+    };
 
     return(
         <Card style={{width: '18rem'}}>
@@ -35,23 +40,22 @@ export default function Support() {
                 <Form ref={form} onSubmit={sendEmail}>
                     
                     <InputGroup className="mb-3">
-                        <Form.Control placeholder="Enter name" name="user_name"/>
+                        <Form.Control placeholder="Enter name" name="user_name" value={name} onChange={e => setName(e.target.value)}/>
                     </InputGroup>
 
                     <InputGroup className="mb-3">
-                        <Form.Control placeholder="Enter email" name="user_email"/>
+                        <Form.Control placeholder="Enter email" name="user_email" value={email} onChange={e => setEmail(e.target.value)}/>
                     </InputGroup>
 
                     <InputGroup className="mb-3">
-                        <Form.Control placeholder="Enter subject" name="subject"/>
+                        <Form.Control placeholder="Enter subject" name="subject" value={subject} onChange={e => setSubject(e.target.value)}/>
                     </InputGroup>
 
                     <InputGroup.Text className="mb-3">
-                        <Form.Control placeholder="Enter message" name="message"/>
+                        <Form.Control placeholder="Enter message" name="message" value={message} onChange={e => setMessage(e.target.value)}/>
                     </InputGroup.Text>
 
                     <Button variant="success" type="submit" value="Send">Submit</Button>
-
                 </Form>
             </Card.Body>    
         </Card>
