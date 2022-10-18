b2a775c83209fb57609caaed38805d1be97548e5
Nathan <nathand1278@gmail.com>
2022-10-18 15:45:14 +1300

    Update Lachlan Support code

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -1,62 +1,87 @@
-import { React, useState} from 'react';
+import { React, useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import { InputGroup } from 'react-bootstrap';
 import { useRef } from 'react';
 
-import emailjs from '@emailjs/browser';
-// import InputGroup from 'react-bootstrap/InputGroup';
+import { firestore } from '../service/firebase';
+import { addDoc, collection } from '@firebase/firestore';
+
+import { userLoggedIn } from '../util/Helpers';
+import { getUserDetails } from '../util/Functions';
 
 export default function Support() {
+    if (!userLoggedIn()) {window.location.href = '/login';}
 
-    const [name, setName] = useState('');
+    	// get userjson
+	const [loading, setLoading] = useState(false);
+	const [userJson, setUserJson] = useState({});
+
+    // create states for each support field
 	const [email, setEmail] = useState('');
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
 
+	// react on page load
+	useEffect(() => {
+		!loading && getUserDetails(sessionStorage.getItem("token"), sessionStorage.getItem("userid")).then(async (response) => {
+			const data = await response.json();
+			setUserJson(data);
+			setLoading(true);
+		});
+
+		if(loading) {
+			setEmail(userJson.email);
+		}
+	}, [loading, userJson]);
+
     const form = useRef();
+
+    const [completed, setCompleted] = useState(false);
 
     const sendEmail = (e) => {
         e.preventDefault();
 
-        // console.log(e.target.user_name);
-        console.log(form.current);
-    
-        emailjs.sendForm('service_li8ozfr', 'template_wijoygh', form.current, '2Db5whLZG7lX83Sc7')
-          .then((result) => {
-              console.log("resX: ", result.text);
-          }, (error) => {
-              console.error("errX: ", error.text);
-          });
+        const ref = collection(firestore, 'mail');
 
-          e.target.reset();
+        let data =             {
+            to: email,
+            message: {
+              subject: 'Support ticket recieved regarding: ' + subject,
+              html: 'Hey, ' + userJson.first_name + ' ' + userJson.last_name + '. We have recieved your support ticket. We will get back to you as soon as possible. <br><br> Your message: <br>' + message,
+            },
+        }
+
+        try {
+            addDoc(ref, data);
+        } catch (err) {
+            console.log(err);
+        }
+
+        setCompleted(true);
     };
 
     return(
         <Card style={{width: '18rem'}}>
             <Card.Body>
                 <Card.Title>Support</Card.Title>
-                <Form ref={form} onSubmit={sendEmail}>
-                    
-                    <InputGroup className="mb-3">
-                        <Form.Control placeholder="Enter name" name="user_name" value={name} onChange={e => setName(e.target.value)}/>
-                    </InputGroup>
+                    {!completed ? (
+                        <Form ref={form} onSubmit={sendEmail}>
+                            <InputGroup className="mb-3">
+                                <Form.Control placeholder="Enter subject" name="subject" value={subject} onChange={e => setSubject(e.target.value)}/>
+                            </InputGroup>
 
-                    <InputGroup className="mb-3">
-                        <Form.Control placeholder="Enter email" name="user_email" value={email} onChange={e => setEmail(e.target.value)}/>
-                    </InputGroup>
+                            <InputGroup.Text className="mb-3">
+                            <Form.Control placeholder="Enter message" name="message" value={message} onChange={e => setMessage(e.target.value)}/>
+                            </InputGroup.Text>
 
-                    <InputGroup className="mb-3">
-                        <Form.Control placeholder="Enter subject" name="subject" value={subject} onChange={e => setSubject(e.target.value)}/>
-                    </InputGroup>
-
-                    <InputGroup.Text className="mb-3">
-                        <Form.Control placeholder="Enter message" name="message" value={message} onChange={e => setMessage(e.target.value)}/>
-                    </InputGroup.Text>
-
-                    <Button variant="success" type="submit" value="Send">Submit</Button>
-                </Form>
+                            <Button variant="success" type="submit" value="Send">Submit</Button>
+                        </Form>
+                    ): (
+                        <h1>Thank you for your message!</h1>
+                    )
+                }
             </Card.Body>    
         </Card>
     );
