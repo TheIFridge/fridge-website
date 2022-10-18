a9fa2738e341768f8f13c7df949c8d3ce4a49f23
Nathan <nathand1278@gmail.com>
2022-10-18 15:44:13 +1300

    Revert "Update Lachlan support code"
    This reverts commit a8eb32043092699395674481b6b6d37ef7c282da.

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -1,87 +1,58 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import { InputGroup } from 'react-bootstrap';
 import { useRef } from 'react';
-
-import { firestore } from '../service/firebase';
-import { addDoc, collection } from '@firebase/firestore';
-
-import { userLoggedIn } from '../util/Helpers';
-import { getUserDetails } from '../util/Functions';
+import emailjs from '@emailjs/browser';
+// import InputGroup from 'react-bootstrap/InputGroup';
 
 export default function Support() {
-    if (!userLoggedIn()) {window.location.href = '/login';}
 
-    	// get userjson
-	const [loading, setLoading] = useState(false);
-	const [userJson, setUserJson] = useState({});
-
-    // create states for each support field
-	const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
-    const [message, setMessage] = useState('');
-
-	// react on page load
-	useEffect(() => {
-		!loading && getUserDetails(sessionStorage.getItem("token"), sessionStorage.getItem("userid")).then(async (response) => {
-			const data = await response.json();
-			setUserJson(data);
-			setLoading(true);
-		});
-
-		if(loading) {
-			setEmail(userJson.email);
-		}
-	}, [loading, userJson]);
+    // const [name, setName] = useState('');
+	// const [email, setEmail] = useState('');
+    // const [subject, setSubject] = useState('');
+    // const [message, setMessage] = useState('');
 
     const form = useRef();
 
-    const [completed, setCompleted] = useState(false);
-
     const sendEmail = (e) => {
         e.preventDefault();
-
-        const ref = collection(firestore, 'mail');
-
-        let data =             {
-            to: email,
-            message: {
-              subject: 'Support ticket recieved regarding: ' + subject,
-              html: 'Hey, ' + userJson.first_name + ' ' + userJson.last_name + '. We have recieved your support ticket. We will get back to you as soon as possible. <br><br> Your message: <br>' + message,
-            },
-        }
-
-        try {
-            addDoc(ref, data);
-        } catch (err) {
-            console.log(err);
-        }
-
-        setCompleted(true);
-    };
+    
+        emailjs.sendForm('service_li8u0xp', 'template_t4nrt2u', form.current, '2Db5whLZG7lX83Sc7')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+      };
 
     return(
         <Card style={{width: '18rem'}}>
             <Card.Body>
                 <Card.Title>Support</Card.Title>
-                    {!completed ? (
-                        <Form ref={form} onSubmit={sendEmail}>
-                            <InputGroup className="mb-3">
-                                <Form.Control placeholder="Enter subject" name="subject" value={subject} onChange={e => setSubject(e.target.value)}/>
-                            </InputGroup>
+                <Form ref={form} onSubmit={sendEmail}>
+                    
+                    <InputGroup className="mb-3">
+                        <Form.Control placeholder="Enter name" name="user_name"/>
+                    </InputGroup>
 
-                            <InputGroup.Text className="mb-3">
-                            <Form.Control placeholder="Enter message" name="message" value={message} onChange={e => setMessage(e.target.value)}/>
-                            </InputGroup.Text>
+                    <InputGroup className="mb-3">
+                        <Form.Control placeholder="Enter email" name="user_email"/>
+                    </InputGroup>
 
-                            <Button variant="success" type="submit" value="Send">Submit</Button>
-                        </Form>
-                    ): (
-                        <h1>Thank you for your message!</h1>
-                    )
-                }
+                    <InputGroup className="mb-3">
+                        <Form.Control placeholder="Enter subject" name="subject"/>
+                    </InputGroup>
+
+                    <InputGroup.Text className="mb-3">
+                        <Form.Control placeholder="Enter message" name="message"/>
+                    </InputGroup.Text>
+
+                    <Button variant="success" type="submit" value="Send">Submit</Button>
+
+                </Form>
             </Card.Body>    
         </Card>
     );
