1b23742d0970a493658025516b2f56b2902cd272
llwong03 <lachlan.ljwong@gmail.com>
2022-10-20 12:48:04 +1300

    Update Recipes.js

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -11,55 +11,58 @@ import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 
 export default function Recipes() {
-    if (!userLoggedIn()) {window.location.href = '/login';}
+    if (!userLoggedIn()) { window.location.href = '/login'; }
 
     const [filter, setFilter] = useState(null);
     const [menuItems, setMenuItems] = useState([]);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        if(!loading) {
+        if (!loading) {
             setMenuItems(Data);
             setLoading(true);
         }
     }, [loading, menuItems, filter]);
-    
+
     return (
         <>
             <h1>Recipe Filtering</h1>
-            <br/>
+            <br />
             <Container>
                 <Row xs={1} md={4} lg={4}>
                     <Col>
-                        <Button variant="success" style={{width: '90%', height: '90%'}} onClick={() => setFilter('Dairy Free')}>Diary Free</Button>
+                        <Button variant="success" style={{ width: '90%', height: '90%' }} onClick={() => setFilter('Dairy Free')}>Diary Free</Button>
                     </Col>
                     <Col>
-                        <Button variant="success" style={{width: '90%', height: '90%'}} onClick={() => setFilter('Gluten Free')}>Gluten Free</Button>
+                        <Button variant="success" style={{ width: '90%', height: '90%' }} onClick={() => setFilter('Gluten Free')}>Gluten Free</Button>
                     </Col>
                     <Col>
-                        <Button variant="success" style={{width: '90%', height: '90%'}} onClick={() => setFilter('Vegetarian')}>Vegetarian</Button>
+                        <Button variant="success" style={{ width: '90%', height: '90%' }} onClick={() => setFilter('Vegetarian')}>Vegetarian</Button>
                     </Col>
                     <Col>
-                        <Button variant="success" style={{width: '90%', height: '90%'}} onClick={() => setFilter('')}>No Filter</Button>
+                        <Button variant="success" style={{ width: '90%', height: '90%' }} onClick={() => setFilter('')}>No Filter</Button>
                     </Col>
                 </Row>
             </Container>
-            <br/>
+            <br />
             <Container>
                 <Row xs={1} md={3} lg={3}>
                     {menuItems.map((item, index) => {
                         // make sure it only shows the items that match the filter
                         if (filter === null || item.category === filter) {
                             return (
-                                <Col key={index}>
-                                    <Card style={{ width: '100%', height: '90%' }}>
-                                        <Card.Img variant="top" src={item.image} />
-                                        <Card.Body>
-                                            <Card.Title>{item.title}</Card.Title>
-                                            <Card.Text>
+                                <Col key={index} md={6} className="mb-5">
+                                    <Card>
+                                        <Card.Body className="d-flex flex-column">
+
+                                            <Card.Img variant="top" src={item.images} className='image-test' />
+                                            <Card.Title className="justify-content-center">{item.title}</Card.Title>
+                                            <Card.Text className="justify-content-center">
                                                 {item.description}
                                             </Card.Text>
-                                            <a href={item.url}><Button variant="info">Go to recipe</Button></a>
+                                            <Card.Footer>
+                                                <Button href={item.url} variant="info">Go to recipe</Button>
+                                            </Card.Footer>
                                         </Card.Body>
                                     </Card>
                                 </Col>
